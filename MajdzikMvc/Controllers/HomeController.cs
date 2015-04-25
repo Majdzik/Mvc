adf78a69beee7680012efb97743976bd77936559
Filip Majdzik <filip@majdzik.com>
2015-04-25 23:05:45 +0200

    js lib config

--- MajdzikMvc/Controllers/HomeController.cs
+++ MajdzikMvc/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace MajdzikMvc.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+                          
             return View();
         }
 
